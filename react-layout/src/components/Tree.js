4bd39211744a56e24ed05679337d8a612f4e20f8
Rithika Boinapalli <rithika.boinapalli@gmail.com>
2024-10-29 12:40:50 -0400

    Toggle Menu

---
+++ react-layout/src/components/Tree.js
@@ -0,0 +1,22 @@
+import "../css/Tree.css";
+import { useState } from "react";
+
+
+const Tree = (tree) => {
+    const [pruned, pruneTree] = useState(false);
+
+    const doTreeJob =() => {
+        pruneTree(true);
+    };
+
+    return (
+        <section class = "tree">
+            <h3 class ={pruned?"pruned":""}> {tree.name}</h3>
+            <p>{pruned}</p>
+            <img src={tree.image} alt={tree.name} />
+            <button onClick ={doTreeJob}>Prune Tree</button>
+        </section>
+    )
+};
+
+export default Tree;
--- react-layout/src/pages/Home.js
+++ react-layout/src/pages/Home.js
@@ -1,5 +1,15 @@
+import Tree from "../components/Tree.js";
+import "../css/Home.css";
+
 const Home = () => {
-    return <h1>Home Page</h1>;
+    return (
+        <div class= "trees">
+        <Tree name ="Bald Cypress" image="images/baldCypress.jpg" />
+        <Tree name="Eastren Redbud"image="redbud.jpg" />
+        <Tree name= "Live Oak" image="images/oak.jpg"/>
+        </div>
+    );
+    
 };
 
 export default Home;
